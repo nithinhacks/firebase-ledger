87135d3ac1fc8f9686e3991ebaa0a03bf7476b4e
nithinhacks <nithinbhpv@gmail.com>
2022-07-01 20:42:47 +0530

    added getTranscations method 🚀

--- controllers/transcationController.js
+++ controllers/transcationController.js
@@ -2,7 +2,7 @@ import { customAlphabet } from "nanoid";
 import { db } from "../firebaseconfig.js";
 import { Timestamp } from "firebase-admin/firestore";
 
-export const makeTranscation = async (req, res) => {
+export const makeTransaction = async (req, res) => {
   const nanoid = customAlphabet("1234567890", 10);
   const id = nanoid();
   const { phone, amountToTransfer } = req.body;
@@ -59,6 +59,34 @@ export const makeTranscation = async (req, res) => {
     });
     await batch.commit();
     res.status(200).json({ message: "Transaction successful" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getTransaction = async (req, res) => {
+  const { cid } = req.user;
+  const walletHistoryRef = db.collection("wallet_history").doc(cid);
+  try {
+    const walletHistory = await walletHistoryRef.listCollections();
+    const result = walletHistory.map(async (collection) => {
+      await collection.get();
+      return collection.id;
+    });
+    const result2 = await Promise.all(result);
+    const transactions = result2.map(async (collection) => {
+      const transaction = await walletHistoryRef.collection(collection).get();
+      return transaction.docs.map((doc) => {
+        const { amount, tran_id, currency, tran_type, status } = doc.data();
+        return {
+          Timestamp: collection,
+          info: { amount, tran_id, currency, tran_type, status },
+        };
+      });
+    });
+    const data = await Promise.all(transactions);
+    const result3 = data.flat();
+    res.status(200).json({ data: result3 });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
