541f6045c8c2eb0c13903b9384be8aa68b27219f
nithinhacks <nithinbhpv@gmail.com>
2022-07-02 15:50:40 +0530

    added env variables 🔥

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,11 +1,21 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+import dotenv from "dotenv";
+dotenv.config();
 
 import admin from "firebase-admin";
-const credentials = require("./firebasekey.json");
 
 export default admin.initializeApp({
-  credential: admin.credential.cert(credentials),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  }),
 });
 
 export const db = admin.firestore();
--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
 import transactionRouter from "./routes/transactionRoutes.js";
 import firebaseconfig from "./firebaseconfig.js";
-
+dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
-dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
